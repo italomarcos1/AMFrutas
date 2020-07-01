c08ca2e2e7ac976820cd1233c1925926afbd898c
Italo M <italomarcos0010@gmail.com>
2020-07-01 11:14:39 -0300

    resolvendo conflitos

--- src/components/ChildrenCategory/index.js
+++ src/components/ChildrenCategory/index.js
@@ -1,69 +1,61 @@
 import React from 'react';
-import { StatusBar, ScrollView, TouchableOpacity } from 'react-native';
+import { StatusBar, ScrollView } from 'react-native';
 import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/Feather';
+import Header from '~/components/Header';
 
 import {
   Container,
   OptionsContainer,
-  Header,
-  SubContainer,
   OptionsTitle,
   Option,
   OptionText,
+  Line,
 } from './styles';
 
-import Logo from '~/assets/logo-white.svg';
-
 export default function ChildrenCategory({ route, navigation }) {
-  const { categories } = route.params;
+  const { categories, categoryName } = route.params;
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#12b118" />
-      <Header>
-        <SubContainer>
-          <TouchableOpacity
-            onPress={() => navigation.goBack()}
-            hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
-          >
-            <Icon size={35} name="chevron-left" color="#EEE" />
-          </TouchableOpacity>
-          <Logo />
-        </SubContainer>
-      </Header>
+      <Header />
       <Container>
         <ScrollView
           contentContainerStyle={{
             backgroundColor: '#12b118',
-            height: 600,
+            flex: 1,
           }}
         >
-          <OptionsContainer style={{ marginTop: 15, height: 540 }}>
-            <OptionsTitle>Produtos</OptionsTitle>
+          <OptionsContainer>
+            <OptionsTitle>{categoryName}</OptionsTitle>
             {categories.map(category => (
-              <Option
-                key={category.id}
-                onPress={() => {
-                  if (category.all_children_categories.length === 0) {
-                    navigation.navigate('Category', {
-                      id: category.id,
-                      title: category.name,
-                    });
-                  } else {
-                    navigation.navigate('ChildrenCategory', {
-                      categories: category.all_children_categories,
-                    });
-                  }
-                }}
-              >
-                <OptionText>{category.name}</OptionText>
-                {category.all_children_categories.length !== 0 ? (
-                  <Icon name="plus" color="#000" size={20} />
-                ) : (
-                  <Icon />
-                )}
-              </Option>
+              <>
+                <Line />
+
+                <Option
+                  key={category.id}
+                  onPress={() => {
+                    if (category.all_children_categories.length === 0) {
+                      navigation.navigate('Category', {
+                        id: category.id,
+                      });
+                    } else {
+                      navigation.navigate('ChildrenCategory', {
+                        categories: category.all_children_categories,
+                        categoryName: category.name,
+                      });
+                    }
+                  }}
+                >
+                  <OptionText>{category.name}</OptionText>
+                  {category.all_children_categories.length !== 0 ? (
+                    <Icon name="plus" color="#000" size={20} />
+                  ) : (
+                    <Icon />
+                  )}
+                </Option>
+              </>
             ))}
           </OptionsContainer>
         </ScrollView>
@@ -76,10 +68,10 @@ ChildrenCategory.propTypes = {
   route: PropTypes.shape({
     params: PropTypes.shape({
       categories: PropTypes.oneOfType([PropTypes.array]),
+      categoryName: PropTypes.string,
     }),
   }).isRequired,
   navigation: PropTypes.shape({
     navigate: PropTypes.func,
-    goBack: PropTypes.func,
   }).isRequired,
 };
