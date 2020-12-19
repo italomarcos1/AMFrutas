bdf1acb118d3d0760777c348bfd97806d5064ede
Ramires Gomes <ramires.gb@gmail.com>
2020-12-18 23:38:30 -0300

    Implementar verificador de versões

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
 import './config/ReactotronConfig';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { NavigationContainer } from '@react-navigation/native';
 
-import { StatusBar } from 'react-native';
+import { Alert, StatusBar, BackHandler, Linking } from 'react-native';
+import Toast from 'react-native-tiny-toast';
+import VersionCheck from 'react-native-version-check';
 import Routes from './routes';
 
 import { store, persistor } from './store';
 
 function Index() {
+  const checkVersion = async () => {
+    try {
+      const updateNeed = await VersionCheck.needUpdate();
+
+      if (updateNeed && updateNeed.isNeeded) {
+        Alert.alert(
+          'Por favor atualize',
+          'Você precisa atualizar o aplicativo para a última versão para continuar comprando.',
+          [
+            {
+              text: 'Atualizar',
+              onPress: () => {
+                BackHandler.exitApp();
+                Linking.openURL(updateNeed.storeUrl);
+              },
+            },
+          ],
+          { cancelable: false }
+        );
+      }
+    } catch (error) {
+      Toast.show(error);
+    }
+  };
+
+  useEffect(() => {
+    checkVersion();
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
